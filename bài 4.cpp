e5099f3e2ef4e0885ecc1669879ef27500baf90b
dai611351 <dai611351@gmail.com>
2024-11-20 21:27:55 +0700

    bai4

---
+++ bài 4.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+int main() {
+    // Khai bao va khoi tao bien canh hinh vuong 
+    
+	//Canh hinh vuong 
+	int side = 3; 
+
+    // Tính chu vi hinh vuong
+    int perimeter = 4 * side;
+
+    // Tính dien tích hinh vuong
+    int area = side * side;
+
+    // Ket qua 
+    printf("Canh hinh vuong: %d\n", side);
+    printf("Chu vi hinh vuong: %d\n", perimeter);
+    printf("Dien tich hinh vuong: %d\n", area);
+
+    return 0;
+}
