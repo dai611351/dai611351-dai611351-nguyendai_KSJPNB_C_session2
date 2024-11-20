ab7c052b5251a115e7d598759b9c5b1d18e02822
dai611351 <dai611351@gmail.com>
2024-11-20 21:27:23 +0700

    bai2

---
+++ bài 2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main() {
+    // Khai bao va khoi tao hai bien so nguyen 
+    
+	// Bien so nguyen thu nhat
+	int num1 = 12; 
+    
+    // Bien so nguyen thu hai
+	int num2 = 4; 
+
+    // Tinh tong hai so va luu ket qua vao bien khac 
+    int total = num1 + num2;
+
+    // Ket qua 
+    printf("Tong: %d + %d = %d", num1, num2, total);
+
+    return 0;
+}
+
